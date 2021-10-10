43ebac7e2a6ee58f3c6675c0901b679ca1dc6182
princeNITian <princeraj427@gmail.com>
2021-10-10 19:25:39 +0530

    add user and disconnect added

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 var app = express();
+var server = http.createServer(app);
 
 const publicPath = path.join(__dirname,'../public')
 const port = process.env.PORT || 3000;
 
+const io = socketIO(server);
+
 app.use(express.static(publicPath))
+
+io.on('connection', (socket) => {
+    console.log('New user connected!')
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected!')
+    })
+})
 
 // app.get('/',(req,res) => {
 //     res.sendFile(publicPath+'/index.html')
 // })
 
-app.listen(port,() => {
+server.listen(port,() => {
     console.log(`Listening on port ${port}`);
 })
 
