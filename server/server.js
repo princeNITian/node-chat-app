7c7918cd7f58c0f64be15196376f92bbab398d1c
princeNITian <princeraj427@gmail.com>
2021-11-09 19:40:53 +0530

    Send message to people in room only

--- server/server.js
+++ server/server.js
@@ -47,10 +47,14 @@ io.on('connection', (socket) => {
     })
 
     socket.on('createMessage',function(message,callback){
-        console.log('createMessage', message);
+        var user = users.getUser(socket.id);
 
+        if (user && isRealString(message.text)) {
         // server emits message to all clients through io
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
+
+       
         callback();
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
@@ -60,7 +64,10 @@ io.on('connection', (socket) => {
     })
 
     socket.on('createLocationMessage', function(coords) {
-        io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude))
+        var user = users.getUser(socket.id);
+        if(user){
+            io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude))
+        }
     })
 
     socket.on('disconnect', () => {
